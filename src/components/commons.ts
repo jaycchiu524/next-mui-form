e693822a8dfe54fb0d95f48cbb2cf6bb47a0c6f2
Jay Chiu <jaycchiu524@gmail.com>
2022-10-22 22:55:44 -0400

    feat: add container

---
+++ src/components/commons.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const Container = styled('div')`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  min-height: 100vh;
+  padding: 15vh 0;
+  background: white;
+`
